6a250b7de8a219f29d0854c0733f4081d687ce6d
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-02-02 17:11:26 +0200

    added
    added

---
+++ app.js
@@ -0,0 +1,48 @@
+﻿async function loadTable(url,table){
+    const tableHead = document.querySelector('thead');
+    const tableBody = document.querySelector('tbody');
+
+    const response = await fetch(url);
+    const data = await  response.json();
+    //console.log(data);
+    const{rows,headers} = data;
+
+
+    // Clear the table
+    tableHead.innerHTML = '<tr></tr>';
+    tableBody.innerHTML = '';
+
+
+    // Populate the headers
+
+    for(const headerText of headers){
+        const headerElement = document.createElement('th');
+        headerElement.textContent = headerText;
+
+        tableHead.querySelector('tr').appendChild(headerElement);
+    }
+
+    //Populate the rows
+
+    for(const row of rows){
+
+        const rowElement = document.createElement('tr');
+
+        for(const cellText of row){
+            const cellElement = document.createElement('td');
+
+            cellElement.textContent = cellText;
+            rowElement.appendChild(cellElement);
+        }
+
+        tableBody.appendChild(rowElement);
+
+    }
+
+
+
+}
+
+loadTable("./data.json",document.querySelector('table') );
+
+//table -> document.querySelector(''table)
